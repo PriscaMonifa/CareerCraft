d8eb2fc8caf428b536ddc68cd3eb89380e6ec0da
Monifa Prisca <drmonifaprisca@gmail.com>
2026-06-19 10:50:50 +0530

    Delete CareerCrafter-Hib directory
    removed duplicate

--- CareerCrafter-Hib/src/main/java/com/enums/Role.java
+++
@@ -1,6 +0,0 @@
-package com.enums;
-
-public enum Role {
-    EMPLOYER,
-    JOB_SEEKER
-}
--- CareerCrafter-Hib/src/main/java/com/exception/InvalidRoleException.java
+++
@@ -1,7 +0,0 @@
-package com.exception;
-
-public class InvalidRoleException extends Throwable {
-    public InvalidRoleException(String message) {
-        super(message);
-    }
-}
